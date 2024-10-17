dcf9d8f8e8ea1a9eb5b92397606d32f9d7cec54e
Marina <mreptru297@g.educaand.es>
2024-10-17 09:37:19 +0200

    creo mis divs

--- E4.php
+++ E4.php
@@ -1,9 +1,10 @@
 <?php 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 echo'hola';
 echo'papariaparairpaidara';
-=======
-    echo"adios";
-    echo"soy marina";
->>>>>>> f16e837ffef3f57bf334734017e97adc9929d7ce
+//=======
+//MARINA
 ?>
+<div></div>
+
+//>>>>>>> f16e837ffef3f57bf334734017e97adc9929d7ce
